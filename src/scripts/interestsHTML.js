b9247448834d998c3bee67d83051da048331094a
Mo Silvera <mo.leslie.silvera@gmail.com>
2019-02-22 13:59:09 -0600

    added 'location' to intrest display html

--- src/scripts/interestsHTML.js
+++ src/scripts/interestsHTML.js
@@ -2,8 +2,8 @@ let interestHTML = (interest) => {
         return `
         <section id="interest--${interest.id}">
             <h2>${interest.name}</h2>
-            <div>${interest.description}</div>
-            <div>${interest.place.name}</div>
+            <p>${interest.description}</p>
+            <div>Location: ${interest.place.name}</div>
             <button id="delete--button--${interest.id}">Delete</button>
         </section>
         `
