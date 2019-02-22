fb579689cf84b34f8d394b7edbd9c20976ebe71a
Mo Silvera <mo.leslie.silvera@gmail.com>
2019-02-22 14:49:32 -0600

    refactored to modularize api handling fucntions and interest printing function

--- src/scripts/interestPrint.js
+++ src/scripts/interestPrint.js
@@ -1,9 +1,9 @@
 import APImanager from "./APImanager"
-import interestsHTML from interestsHTML
+import interestsHTML from "./interestsHTML"
 let listContainer = document.getElementById("listContainer")
 const interestPrint = () => {
     APImanager.fetchInterests()
-    .then.then((interestArray) => {interestArray.forEach(interest => {
+    .then((interestArray) => {interestArray.forEach(interest => {
         listContainer.innerHTML += interestsHTML(interest)
         })
     }
